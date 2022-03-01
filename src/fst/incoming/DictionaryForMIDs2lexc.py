18cb8c4e236f1e6fabf88aa829901808e7d18657
Nathan Adamson <97128520+adamsnat007@users.noreply.github.com>
2022-03-01 14:48:47 -0700

    Dictionay parsing python code

--- src/fst/incoming/DictionaryForMIDs2lexc.py
+++ src/fst/incoming/DictionaryForMIDs2lexc.py
@@ -3,6 +3,9 @@
 import re
 from zipfile import ZipFile
 
+trans_table = [('á', 'á'),
+               ('í', 'í')]
+
 lines = []
 with ZipFile('DictionaryForMIDs/DictionaryForMIDs_HilEng_KVED.jar') as zf:
     for fname in zf.namelist():
@@ -16,7 +19,16 @@ cc = 'ANV'  # TODO name the continuation class (this idea stands from AdjectiveN
 with open('dfm.lexc', 'w') as f:
     print('LEXICON DictionaryForMIDs_roots', file=f)
     for line in lines:
-        # TODO add logic here to parse each line
-        stem = ''
-        gloss = ''
+        line = line.decode('utf8')
+        stem, gloss = line.split('\t')
+        for find, replace in trans_table:
+            if find in stem:
+                print(find, stem)
+                stem.replace(find, replace)
+                assert find not in replace
+        gloss = re.sub(r'\[01([^\]]+)\]', r'\1', gloss)
+        gloss = re.split(r'(?<!Sp)[!,;.]|\\n', gloss)[0].strip()
+        if '[01' in gloss:
+            print('DEBUG', repr(line), gloss, gloss)
+        gloss = gloss.replace('"', '%"')
         print(f'{stem} {cc} "{gloss}" ;', file=f)
