0bfe9d282265f720a35e74d7eb143cb036db19df
Nathan Adamson <97128520+adamsnat007@users.noreply.github.com>
2022-03-31 13:55:11 -0600

    Added flags to content and updated python script

--- src/fst/incoming/DictionaryForMIDs2lexc.py
+++ src/fst/incoming/DictionaryForMIDs2lexc.py
@@ -1,5 +1,6 @@
 """Convert DictionaryForMIDs dictionary to lexc entries."""
 
+from collections import defaultdict
 import re
 from zipfile import ZipFile
 
@@ -45,26 +46,35 @@ with ZipFile('DictionaryForMIDs/DictionaryForMIDs_HilEng_KVED.jar') as zf:
 
 cc = 'ANV'  # TODO name the continuation class (this idea stands from AdjectiveNounVerb)
 with open('content.lexd', 'w') as f:
-    print('LEXICON Content', file=f)
+    print('LEXICON Stem(5)', file=f)
+    flag_dict = defaultdict(list)
     for line in lines:
         line = line.decode('utf8')
         stem, gloss = line.split('\t')
         new_stem = stem.split(',')[0]
         if new_stem.startswith('-'):
             continue
-        try:
-            s1, s2, s3 = re.match(r'([-bcdfghjklmnpqrstvwxyz]*)([aeiouáàâéèêíìîóòôúùû])([-áàâéèêíìîóòôúùûa-z]*)$', new_stem, flags=re.I).groups()
-        except AttributeError:
-            print(new_stem)
-            continue
         for find, replace in trans_table:
             if re.search(find, new_stem):
                 #print(find, replace, stem)
                 new_stem = re.sub(find, replace, new_stem)
                 assert not re.search(find, new_stem)
+        flag_stem = new_stem + str(len(flag_dict[new_stem]))
+        flag_dict[new_stem].append(flag_stem)
+        try:
+            s1, s2, s3 = re.match(r'([-bcdfghjklmnpqrstvwxyz%^]*)([aeiou\u0301])([-\u0301a-z%^]*)$', new_stem, flags=re.I).groups()
+            if not s1:
+                s1 = ':'
+            if not s2:
+                raise ValueError(stem)
+            if not s3:
+                s3 = ':'
+        except AttributeError:
+            print(new_stem)
+            continue
         gloss = re.sub(r'\[01([^\]]+)\]', r'\1', gloss)
         gloss = re.split(r'(?<!Sp)[!,;.]|\\n', gloss)[0].strip()
         if '[01' in gloss:
             print('DEBUG', repr(line), gloss, gloss)
         gloss = gloss.replace('"', '%"')
-        print(f'{s1} {s2} {s3}   # {gloss} !{{{stem}}}', file=f)
+        print(f'{s1} {s2} {s3} <@P.LEX.{flag_stem}@> <@R.LEX.{flag_stem}@>  # {gloss} !{{{stem}}}', file=f)
