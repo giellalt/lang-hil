af20ec66be3d8db05cb6e5400a871158f64847d6
Sjur N Moshagen <sjurnm@mac.com>
2024-01-24 10:12:35 +0200

    [Template merge] Adjust path after fst build reorg

--- devtools/generate-adj-wordforms.sh
+++ devtools/generate-adj-wordforms.sh
@@ -22,13 +22,13 @@ fi
 morf_codes="+A"
 
 # Lexicon source file for lexicons and lemmas:
-source_file=src/morphology/stems/adjectives.lexc
+source_file=src/fst/morphology/stems/adjectives.lexc
 
 # Lexicons that should NOT be used to extract lemmas (egrep expression):
 exception_lexicons="(flagK)"
 
 # FST used for generation, MINUS suffix:
-generator_file=src/generator-gt-norm
+generator_file=src/fst/generator-gt-norm
 
 # How many lemmas maximally for each lexicon:
 lemmacount=10
